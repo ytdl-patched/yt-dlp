98c5f910c295e29810ad47b8a6cc4e7fd52c48b8
Lesmiscore <nao20010128@gmail.com>
2022-05-17 13:26:56 +0900

    Automated Daily Builds

--- yt_dlp/update.py
+++ yt_dlp/update.py
@@ -23,7 +23,7 @@ from .utils import (
 )
 from .version import UPDATE_HINT, VARIANT, __version__
 
-REPOSITORY = 'yt-dlp/yt-dlp'
+REPOSITORY = 'ytdl-patched/yt-dlp'
 API_URL = f'https://api.github.com/repos/{REPOSITORY}/releases'
 
 
